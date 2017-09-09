0b5a16d5ff8652817b8a5a0daa2678423ab3d426
ANISH SAMANT <anish.samant97@gmail.com>
2017-09-09 20:13:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,6 +112,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var name=[];
 app.get('/submit-name/:name',function(req,res){
     //get the name from request object
     var name=req.params.name;
