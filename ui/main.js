addd913fec907199caf97704de16e603b7d9bd8b
ANISH SAMANT <anish.samant97@gmail.com>
2017-09-09 18:50:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,9 +6,9 @@ element.innerHTML="NEW VALUE";
 var img=document.getElementById('madi');
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+5;
     img.style.marginleft=marginLeft+'px';
 }
 img.onclick=function(){
-    var interval=setInterval(moveRight,100);
+    var interval=setInterval(moveRight,50);
 };
