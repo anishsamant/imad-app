332e9bfebbef76f686513aff7ac309d6dbfeaf52
ANISH SAMANT <anish.samant97@gmail.com>
2017-09-09 20:06:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ button.onclick=function(){
 };
 
 //submit name
-var inputButton=document.getElementById('name');
+var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
