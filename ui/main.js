1ce65654571446ffb9a495cca92418e037299abd
ANISH SAMANT <anish.samant97@gmail.com>
2017-09-09 19:52:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -31,5 +31,11 @@ var submit=document.getElementByid('submit_btn');
 submit.onclick=function(){
     //make request to the server and send the name
     //capture a list of names and render it as a list
-    
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
 };
